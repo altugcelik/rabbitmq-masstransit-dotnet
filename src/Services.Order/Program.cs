2c1ad9a218ec397d9b422936a3e394102f679d17
Altug Celik <32844205+altugcelik@users.noreply.github.com>
2025-12-19 15:52:14 +0300

    new commit

---
+++ src/Services.Order/Program.cs
@@ -0,0 +1,7 @@
+using Services.Order;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/Services.Stock/Program.cs
@@ -0,0 +1,7 @@
+using Services.Stock;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
