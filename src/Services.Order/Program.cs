9dba678ad9e048aeda400dd2a9a12cfd8d2aabe9
Altug Celik <32844205+altugcelik@users.noreply.github.com>
2025-12-19 17:43:02 +0300

    .

--- src/Services.Order/Program.cs
+++ src/Services.Order/Program.cs
@@ -3,31 +3,44 @@ using Microsoft.EntityFrameworkCore;
 using Services.Order.Consumers;
 using Services.Order.Outbox;
 
-Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
+var builder = WebApplication.CreateBuilder(args);
+
+// DB
+builder.Services.AddDbContext<OrderDbContext>(opt =>
+    opt.UseNpgsql("Host=localhost;Port=5432;Database=saga;Username=saga;Password=saga"));
+
+// MassTransit
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<CreateOrderConsumer>();
+
+    x.UsingRabbitMq((ctx, cfg) =>
     {
-        services.AddDbContext<OrderDbContext>(opt =>
-            opt.UseNpgsql(
-                "Host=localhost;Port=5432;Database=saga;Username=saga;Password=saga"));
-    
-        services.AddMassTransit(x =>
+        cfg.Host("localhost", "/", h =>
         {
-            x.AddConsumer<CreateOrderConsumer>();
-
-            x.UsingRabbitMq((ctx, cfg) =>
-            {
-                cfg.Host("localhost", "/", h =>
-                {
-                    h.Username("appuser");
-                    h.Password("apppassword");
-                });
-
-                cfg.ConfigureEndpoints(ctx);
-            });
+            h.Username("guest");
+            h.Password("guest");
         });
 
-        services.AddHostedService<DatabaseInitializer>();
-        services.AddHostedService<OutboxPublisherWorker>();
-    })
-    .Build()
-    .Run();
+        cfg.ConfigureEndpoints(ctx);
+    });
+});
+
+// Controllers
+builder.Services.AddControllers();
+
+// Outbox Worker
+builder.Services.AddHostedService<OutboxPublisherWorker>();
+
+var app = builder.Build();
+
+// ❗ DB CREATE (DEV ONLY)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    db.Database.EnsureCreated();
+}
+
+app.MapControllers();
+
+app.Run();
